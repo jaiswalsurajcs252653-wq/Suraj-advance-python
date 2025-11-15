b6c59e7030977d5096a314ead48690f1187a5d7d
jaiswalsurajcs252653-wq <jaiswalsurajcs252653@gmail.com>
2025-11-15 10:25:24 +0530

    Add files via upload

---
+++ F089 SURAJ/suraj3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int num1, num2, sum;
+cout <<"Enter Your First Number: " << endl;
+cin >> num1;
+cout <<"Enter Your Second Number: " << endl;
+cin >> num2;
+sum = num1 + num2;
+
+cout<<"sum of num1 & num2 is: " <<sum ;
+
+cout<<"Name:Suraj Jaiswal\n";
+
+cout<<"Roll No:F089\n";
+return 0;}
